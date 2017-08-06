b19fea38ec5c8c0ad23ac0c8e5398295b510baa3
Peter <ppkrauss@gmail.com>
2017-08-06 10:50:05 -0300

    repo etc

--- src/step02-struct.sql
+++ src/step02-struct.sql
@@ -1,4 +1,24 @@
 
+CREATE TABLE repository (
+  id    integer NOT NULL PRIMARY KEY,
+  name  text   NOT NULL,
+  label text   NOT NULL, -- lower case ASCII short-name, no spaces.
+  url   text,  -- official URL (as Wikipedia)
+  dtds  text[],
+  info jsonb,
+  UNIQUE (name),
+  UNIQUE (label)
+);
+
+CREATE TABLE dtd (
+  id serial NOT NULL PRIMARY KEY,
+  label text NOT NULL, -- ex. jats-1.1 from "JATS-v1.1", html-5 from "HTML-v5.0", etc.
+  dtd_url text NOT NULL, -- ex. jats-1.1=ftp://ftp.ncbi.nih.gov/pub/jats/publishing/1.1/
+  info JSONb, -- additional information as full-name, dtd-URL, xml-schema-url, authority-url, etc.
+  kx   JSONb,  -- cache with stats, etc.
+  UNIQUE(label)
+);
+
 CREATE TABLE journal (
   issn integer NOT NULL PRIMARY KEY,
   country text NOT NULL, -- 2-letter country code
@@ -9,12 +29,11 @@ CREATE TABLE journal (
   UNIQUE(country,abbrev)
 );
 
-CREATE TABLE dtd (
-  id serial NOT NULL PRIMARY KEY,
-  abbrev text, -- ex. JATS-v1.1, HTML-v5.0
-  info JSONb,
-  kx   JSONb,  -- cache
-  UNIQUE(abbrev)
+CREATE TABLE journal_repository ( -- provenience of articles and control of valid issn in a repo
+  jrepo_id       serial NOT NULL PRIMARY KEY,
+  issnl          int    NOT NULL, -- references issn.intcode(issn_l)
+  repository_id  int    NOT NULL REFERENCES repository(id),
+  UNIQUE (issnl,repository_id)
 );
 
 CREATE TABLE article (
@@ -22,9 +41,21 @@ CREATE TABLE article (
   issn integer  REFERENCES journal(issn), -- can be null when created
   filename text,  -- official 
   content xml,  -- JATS
+  jrepo_id bigint references journal_repository(jrepo_id), -- as content provenience
   dtd  int REFERENCES dtd(id),
   info JSONb,
-  kx   JSONb  -- cache
+  kx   JSONb,  -- cache
+  UNIQUE(issn,filename) -- only one dtd per article. 
 );
 
+CREATE TABLE article_alt ( -- second dtd or second repo
+  id serial NOT NULL PRIMARY KEY,
+  id_ref bigint NOT NULL references article(id),
+  filename text,  -- official in the new repo
+  content xml,  -- JATS
+  dtd  int REFERENCES dtd(id),
+  info JSONb,
+  kx   JSONb,  -- cache
+  UNIQUE(issn,filename) -- only one dtd per article. 
+);
 
