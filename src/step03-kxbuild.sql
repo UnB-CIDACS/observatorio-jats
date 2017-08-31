941533c85d92910dcf23ab44f6eb69d56dcfa6e7
User <ppkrauss@gmail.com>
2017-08-31 18:28:07 -0300

    kx fields

--- src/step03-kxbuild.sql
+++ src/step03-kxbuild.sql
@@ -12,42 +12,44 @@ CREATE VIEW kx.vw_article_metas1 AS
   -- FALTA lang do artigo e sua DTD, além de relacionar com repositório-origem (autoridade do dump obtido)
    -- substituir por função que captura direto todos os Xpaths pro JSON. Vantagem aqui pode ser otimização
    -- interna do PostgreSQL que criaria o DOM uma vez só para todos... Por ser cache não apita muito.
-   (xpath('count(//article)', content))[1]::text::integer as n_articles,
+   (xpath('count(//article)', content))[1]::text::integer 		as n_articles,
    (xpath('//article/front//pub-date/year/text()', content))[1]::text::integer as year,
-   trim((xpath('//article/front//volume/text()', content))[1]::text) as volume,
-   trim((xpath('//article/front//issue/text()', content))[1]::text) as issue,
+   trim((xpath('//article/front//volume/text()', content))[1]::text) 	as volume,
+   trim((xpath('//article/front//issue/text()', content))[1]::text) 	as issue,
    lib.trim2( xpath('//article/front//article-title//text()',content) ) as title,
-   lib.trim2( xpath('//article/front//abstract//text()', content) ) as abstract,
+   lib.trim2( xpath('//article/front//abstract//text()', content) ) 	as abstract,
    trim((xpath('//article/front//pub-date[@pub-type="epub"]/text()', content))[1]::text) as epub_date,
-   xmlexists('//article/body' PASSING BY REF content) as has_body,
-   xmlexists('//article/front//permissions' PASSING BY REF content) as has_permiss,
-   (xpath('count(//article/body//table)', content))[1]::text::integer as n_btables,
-   (xpath('count(//article/body//fig)', content))[1]::text::integer as n_bfigs,
+   xmlexists('//article/body' PASSING BY REF content)            	as has_body,
+   xmlexists('//article/front//permissions' PASSING BY REF content) 	as has_permiss,
+   (xpath('count(//article/body//table)', content))[1]::text::integer 	as n_btables,
+   (xpath('count(//article/body//fig)', content))[1]::text::integer 	as n_bfigs,
    (xpath('count(//article/back/ref-list/ref)', content))[1]::text::integer as n_refs,
-   char_length(content::text) as nchars_xml,
+   char_length(content::text)                                 		as nchars_xml,
    char_length(  array_to_string( (xpath('//article//text()', content))::text[] ,'')  )  as nchars_txt
   FROM core.article
 ;
 
 
 CREATE VIEW kx.vw_article_metas1_sql AS
-  -- to be used after update of cache!
-   SELECT id, issnl,
+   SELECT *,
     (kx->>'n_articles')::int as n_articles,
     (kx->>'year')::int as year,
     (kx->>'volume')::text as volume,
     lib.lpad0((kx->>'issue')::text) as issue,
     (kx->>'title')::text as title,
-    (kx->>'has_permiss')::boolean as has_body,
+    (kx->>'abstract')::text as abstract,
+    (kx->>'title')::text as epub_date,
+    (kx->>'has_body')::boolean as has_body,
     (kx->>'has_permiss')::boolean as has_permiss,
     (kx->>'n_btables')::int as n_btables,
     (kx->>'n_refs')::int as n_refs,
     (kx->>'n_bfigs')::int as n_bfigs,
     (kx->>'nchars_txt')::int as nchars_txt,
     (kx->>'nchars_xml')::int as nchars_xml
-   FROM core.vw_article_journal_repo
+   FROM core.vw_article_journal_repo --core.article
 ;
 
+-- revisar
 CREATE VIEW kx.vw_article_issue AS
   SELECT a.*, m.issnl, m.year, m.volume, m.issue, m.title,
          m.n_refs, m.has_body, m.has_permiss, m.n_bfigs, m.n_btables, m.nchars_txt, m.nchars_xml
